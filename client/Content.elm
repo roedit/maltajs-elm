34677a033f479cbd0acdfea0d9b20ac3f27778ba
Pietro909 <pietro.grandi.3d@gmail.com>
2016-09-30 17:17:06 +0200

    add docs and email regex

--- client/Content.elm
+++ client/Content.elm
@@ -1,8 +1,24 @@
 module Content exposing (..)
 
+{-| This module hosts the actual content of the page. Thus can be edited right here,
+    using Markdown. 
+
+# Definition
+@docs Content
+
+# Common Helpers
+@docs aboutView, venueView, eventView
+
+-}
+
 import Html exposing (Html, h1, div, p, text)
 import Markdown
 
+
+{-| Renders the HTML for the About section: what is MaltaJS 
+
+    aboutView
+-}
 aboutView : Html a
 aboutView =
   Markdown.toHtml []
@@ -13,6 +29,10 @@ now is open to all the developers on the island.
 Our aim is to provide a place to share knowledge and technologies, through meetings and talks.
   """
 
+{-| Renders the HTML for the Venue section: where there will be the event
+ 
+    venueView
+-}
 venueView : Html a
 venueView =
   Markdown.toHtml []
@@ -20,6 +40,10 @@ venueView =
 This event will be hosted by Microsoft.
   """
     
+{-| Renders the HTML for the Event section: what are we talking about.
+
+    eventView
+-}
 eventView : Html a
 eventView =
   Markdown.toHtml []
--- client/Main.elm
+++ client/Main.elm
@@ -1,4 +1,4 @@
-import Html exposing (Html, text, button, div, section, article, h1, p, a, header, ol, li, h2, text, form, input, label, fieldset, img)
+import Html exposing (Html, text, button, div, section, article, h1, p, a, header, ol, li, h2, text, form, input, label, fieldset, img, span)
 
 import Html.App as App
 import Html.Events exposing (onClick, on, onInput)
@@ -7,36 +7,36 @@ import Http
 import Task exposing (Task)
 import Json.Decode exposing (list, string)
 import String exposing (join, isEmpty)
+import Regex
 
 import Content exposing (..)
 
 main = App.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+  { init = init
+  , view = view
+  , update = update
+  , subscriptions = subscriptions
+  }
 
 -- MODEL
 type alias Model = 
-    { name : String
-    , surname : String
-    , email : String
-    , registered : Bool
-    , signed : Bool
-    , error : String
-    }
+  { name : String
+  , surname : String
+  , email : String
+  , registered : Bool
+  , signed : Bool
+  , error : String
+  }
 
 initialModel : Model
 initialModel =
-    { name = ""
-    , surname = ""
-    , email = ""
-    , registered = False
-    , signed = False
-    , error = ""
-    }
-
+  { name = ""
+  , surname = ""
+  , email = ""
+  , registered = False
+  , signed = False
+  , error = ""
+  }
 
 init : ( Model, Cmd Msg )
 init =
@@ -45,120 +45,145 @@ init =
 
 -- UPDATE
 type Msg
-    = Name String
-    | Surname String
-    | Email String
-    | Register
-    | PostSucceed String
-    | PostFail Http.Error
+  = Name String
+  | Surname String
+  | Email String
+  | Register
+  | PostSucceed String
+  | PostFail Http.Error
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-        Name name ->
-            ( { model | name = name }, Cmd.none )
-        Surname surname ->
-            ( { model | surname = surname }, Cmd.none )
-        Email email ->
-            ( { model | email = email }, Cmd.none )
-        Register ->
-            ( { model | signed = True }, registerMe model )
-        PostSucceed result ->
-            ( { model | registered = True }, Cmd.none )
-        PostFail error ->
-            ( { model | error = "Sorry, there was an error." }, Cmd.none )
+  case msg of
+    Name name ->
+      ( { model | name = name }, Cmd.none )
+    Surname surname ->
+      ( { model | surname = surname }, Cmd.none )
+    Email email ->
+      ( { model | email = email }, Cmd.none )
+    Register ->
+      ( { model | signed = True }, registerMe model )
+    PostSucceed result ->
+      ( { model | registered = True }, Cmd.none )
+    PostFail error ->
+      ( { model | error = "Sorry, there was an error." }, Cmd.none )
 
 
 -- VIEW
 
 formView : Model -> Html Msg
 formView model =
-    form [ id "signup-form", class "container-fluid" ]  
-                [ fieldset [ class "row"] 
-                    [ label [ for "name", class "col-xs-4" ] [ text "Name: " ]
-                    , input 
-                        [ id "name"
-                        , type' "text"
-                        , class "col-xs-4"
-                        , value model.name
-                        , required True
-                        , onInput Name
-                        ] []
-                    ]
-                , fieldset [ class "row"] 
-                    [ label [ for "surname", class "col-xs-4" ] [ text "Surname: " ]
-                    , input 
-                        [ id "surname"
-                        , type' "text"
-                        , class "col-xs-4"
-                        , value model.surname
-                        , required True
-                        , onInput Surname
-                        ] []
-                    ]
-                , fieldset [ class "row"] 
-                    [ label [ for "email", class "col-xs-4" ] [ text "email: " ]
-                    , input 
-                        [ id "email"
-                        , type' "email"
-                        , class "col-xs-4"
-                        , value model.email
-                        , required True
-                        , onInput Email
-                        ] []
-                    ]
-                ]
+  form [ id "signup-form", class "container-fluid" ]  
+    [ fieldset [ class "row"] 
+      [ label [ for "name", class "col-xs-4" ] [ text "Name: " ]
+      , input 
+        [ id "name"
+        , type' "text"
+        , class "col-xs-4"
+        , value model.name
+        , required True
+        , onInput Name
+        ] []
+      ]
+    , fieldset [ class "row"] 
+      [ label [ for "surname", class "col-xs-4" ] [ text "Surname: " ]
+      , input 
+        [ id "surname"
+        , type' "text"
+        , class "col-xs-4"
+        , value model.surname
+        , required True
+        , onInput Surname
+        ] []
+      ]
+    , fieldset [ class "row"] 
+      [ label [ for "email", class "col-xs-4" ] [ text "Email: " ]
+      , input 
+        [ id "email"
+        , type' "email"
+        , class "col-xs-4"
+        , value model.email
+        , required True
+        , onInput Email
+        ] []
+      ]
+   ]
+
+alertView : Model -> Html a
+alertView model =
+  let
+    classes =
+      if (isFormInvalid model) then "alert alert-danger small" 
+      else "hide"
+  in
+    div [ class classes ]
+      [ span [ class "glyphicon glyphicon-exclamation-sign" ] []
+      , text "please fill in all the required fields" 
+      ]
+
 
 view : Model -> Html Msg
 view model =
-    article [ class "container-fluid" ]
-        [ header []
-            [ headerView "" ]
-        , section []
-            [ h1 [][ text "Home and banner here"]
-            , img [ src "malta.jpg" ] []
-            ]
-        , section []
-            [ h1 [ id "event" ] [ text "Event description"]
-            , eventView
-            ]
-        , section []
-            [ h1 [ id "registration" ] [ text "Registration"]
-            , h2 [] [ text "MaltaJS event" ]
-            , formView model
-            , button [ onClick Register, disabled (isFormInvalid model) ] [ text "Sign Up!" ]
-            ]
-        , section []
-            [ h1 [ id "venue" ] [ text "Venue"]
-            , venueView
-            ]
-        , section []
-            [ h1 [ id "about" ] [ text "MaltaJS"]
-            , aboutView
-            ]
-        ]
+  article [ class "container-fluid" ]
+    [ header []
+      [ headerView "" ]
+    , section []
+      [ h1 [][ text "Home and banner here"]
+      , img [ src "malta.jpg" ] []
+      ]
+    , section []
+      [ h1 [ id "event" ] [ text "Event description"]
+      , eventView
+      ]
+    , section []
+      [ h1 [ id "registration" ] [ text "Registration"]
+      , h2 [] [ text "MaltaJS event" ]
+      , formView model
+      , alertView model
+      , button [ onClick Register, disabled (isFormInvalid model) ] [ text "Sign Up!" ]
+      ]
+    , section []
+      [ h1 [ id "venue" ] [ text "Venue"]
+      , venueView
+      ]
+    , section []
+      [ h1 [ id "about" ] [ text "MaltaJS"]
+      , aboutView
+      ]
+    ]
 
-isFormInvalid model = isEmpty model.name || isEmpty model.surname || isEmpty model.email
+isFormInvalid model = 
+  let
+    -- from http://emailregex.com/
+    regex = """[-a-z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-z0-9~!$%^&*_=+}{\\'?]+)*@([a-z0-9_][-a-z0-9_]*(\\.[-a-z0-9_]+)*
+            \\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}
+            \\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}))(:[0-9]{1,5})?$"""
+      |> Regex.regex
+      |> Regex.caseInsensitive
+  in
+    (isEmpty model.name || isEmpty model.surname || isEmpty model.email) || not (Regex.contains regex model.email)
+
 isFormValid model = not (isFormInvalid model)
 
+-- todo: handle selected section
 headerView : String -> Html a 
 headerView selected = 
-    ol [ class "breadcrumb" ]
-    [ li [ class selected ] [ a [ href "#about" ] [ text "About" ] ]
-    , li [] [ a [ href "#event" ] [ text "Event" ] ]
-    , li [] [ a [ href "#registration" ] [ text "Registration" ] ]
-    , li [] [ a [ href "#venue" ] [ text "Venue" ] ]
-    ]
+  ol [ class "breadcrumb" ]
+  [ li [] [ a [ href "#about" ] [ text "About" ] ]
+  , li [] [ a [ href "#event" ] [ text "Event" ] ]
+  , li [] [ a [ href "#registration" ] [ text "Registration" ] ]
+  , li [] [ a [ href "#venue" ] [ text "Venue" ] ]
+  ]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+  Sub.none
 
 
 registerMe : Model -> Cmd Msg
 registerMe model =
-    let
-        url = "http://localhost:8001"
-    in
-        Task.perform PostFail PostSucceed (Http.post string url Http.empty) 
+  let
+    url = "http://localhost:8001"
+  in
+    Task.perform PostFail PostSucceed (Http.post string url Http.empty) 
